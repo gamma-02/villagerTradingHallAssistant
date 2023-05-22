18e4d94f8ac81c527dd4fb27e9de6304c52b0b60
gamma02 <gammagames06@gmail.com>
2023-05-21 19:10:35 -0700

    good god my power is infininte.
    This version of the mod adds support for Trade Cycling, only on fabric.
    use it wisely.

--- src/main/java/gamma02/villagertradinghallassistant/config/Configs.java
+++ src/main/java/gamma02/villagertradinghallassistant/config/Configs.java
@@ -9,6 +9,7 @@ import fi.dy.masa.malilib.config.IConfigBoolean;
 import fi.dy.masa.malilib.config.IConfigHandler;
 import fi.dy.masa.malilib.config.options.ConfigBoolean;
 import fi.dy.masa.malilib.config.options.ConfigBooleanHotkeyed;
+import fi.dy.masa.malilib.config.options.ConfigInteger;
 import fi.dy.masa.malilib.config.options.ConfigStringList;
 import fi.dy.masa.malilib.util.FileUtils;
 import fi.dy.masa.malilib.util.JsonUtils;
@@ -27,7 +28,9 @@ public class Configs implements IConfigHandler {
 
     public static final ConfigBooleanHotkeyed ENABLE_MOD = new ConfigBooleanHotkeyed("enableMod", false, "F,E", "enables finding the villager trade");
 
-    public static final List<IConfigBase> CONFIGS = List.of(AUTO_FIND_WORKSTATION, ACCEPTABLE_ENCHANTMENTS, ENABLE_MOD);
+    public static final ConfigInteger MAX_COST = new ConfigInteger("maxTradeCost", 64, 0, 64, "this is a cutoff for how expensive a trade should be at max");
+
+    public static final List<IConfigBase> CONFIGS = List.of(AUTO_FIND_WORKSTATION, ACCEPTABLE_ENCHANTMENTS, ENABLE_MOD, MAX_COST);
 
 
     public static void loadFromFile() {
@@ -40,7 +43,7 @@ public class Configs implements IConfigHandler {
                 JsonObject obj = element.getAsJsonObject();
                 ConfigUtils.readConfigBase(obj, "Config", CONFIGS);
                 ConfigUtils.readConfigBase(obj, "Hotkeys", Hotkeys.HOTKEYS);
-                ENABLE_MOD.setBooleanValue(false);
+                ENABLE_MOD.resetToDefault();
             }
         }
     }
--- src/main/java/gamma02/villagertradinghallassistant/feature/MainMod.java
+++ src/main/java/gamma02/villagertradinghallassistant/feature/MainMod.java
@@ -1,5 +1,8 @@
 package gamma02.villagertradinghallassistant.feature;
 
+import de.maxhenkel.tradecycling.FabricTradeCyclingClientMod;
+import de.maxhenkel.tradecycling.FabricTradeCyclingMod;
+import de.maxhenkel.tradecycling.TradeCyclingClientMod;
 import gamma02.villagertradinghallassistant.VillagerTradingHallAssistant;
 import gamma02.villagertradinghallassistant.config.Configs;
 import net.minecraft.block.Block;
@@ -9,6 +12,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.MerchantScreen;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.command.argument.EntityAnchorArgumentType;
 import net.minecraft.entity.EquipmentSlot;
@@ -18,6 +22,8 @@ import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtFloat;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+import net.minecraft.screen.MerchantScreenHandler;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
@@ -28,9 +34,11 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOfferList;
 import net.minecraft.village.VillagerProfession;
 
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public class MainMod {
     public static MainMod instance = new MainMod();
@@ -42,87 +50,112 @@ public class MainMod {
 
     public Block workstation = null;
 
-    public boolean toolWarning = true;
+    public boolean hasRefreshedTrades = false;
 
-    public boolean hasBrokenBlock = true;
+    public boolean toRefreshTrades = false;
+
+    public TradeOfferList trades = null;
 
 
     public void tick(){
 
         ClientWorld world = MinecraftClient.getInstance().world;
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
-
-        if(VillagerTradingHallAssistant.workstation != null && world.getBlockState(VillagerTradingHallAssistant.workstation).getBlock() == Blocks.AIR && toBreakWorkstation){
-            toBreakWorkstation = false;
-            hasBrokenBlock = true;
-        }
-
-        if(VillagerTradingHallAssistant.villager != null && MinecraftClient.getInstance().currentScreen == null && !toBreakWorkstation){
+//
+//        if(VillagerTradingHallAssistant.workstation != null && world.getBlockState(VillagerTradingHallAssistant.workstation).getBlock() == Blocks.AIR && toBreakWorkstation){
+//            toBreakWorkstation = false;
+//            hasBrokenBlock = true;
+//        }
+//
+//        if(Configs.ENABLE_MOD.getBooleanValue()){
+//
+//        }
+        if(VillagerTradingHallAssistant.villager != null && MinecraftClient.getInstance().currentScreen == null && Configs.ENABLE_MOD.getBooleanValue())/*just an extra check*/{
             MinecraftClient.getInstance().interactionManager.interactEntity(player, VillagerTradingHallAssistant.villager, player.getActiveHand());
         }
-
-
-
-
-        if(VillagerTradingHallAssistant.workstation != null) {
-
-//            if(toBreakWorkstation && MinecraftClient.getInstance().currentScreen != null){
-//                MinecraftClient.getInstance().currentScreen.close();
+//
+//
+//
+//
+//        if(VillagerTradingHallAssistant.workstation != null) {
+//
+////            if(toBreakWorkstation && MinecraftClient.getInstance().currentScreen != null){
+////                MinecraftClient.getInstance().currentScreen.close();
+////            }
+//
+//
+//            if (toBreakWorkstation && MinecraftClient.getInstance().currentScreen == null) {
+//                workstation = world.getBlockState(VillagerTradingHallAssistant.workstation).getBlock();
+//                if(!switchToEffectiveTool(world.getBlockState(VillagerTradingHallAssistant.workstation), player)){
+//                    if(toolWarning) {
+//                        MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("No effective tool in the hotbar! please put the effective tool for the workstation in your hotbar."));
+//                        toolWarning = false;
+//                    }
+//                    return;
+//                }
+//                player.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES, new Vec3d(VillagerTradingHallAssistant.workstation.getX() + 0.5, VillagerTradingHallAssistant.workstation.getY()+0.5, VillagerTradingHallAssistant.workstation.getZ() + 0.5));
+//                MinecraftClient.getInstance().options.attackKey.setPressed(true);
+////                Objects.requireNonNull(MinecraftClient.getInstance().interactionManager).attackBlock(VillagerTradingHallAssistant.workstation, Direction.UP);
+//                hasBrokenBlock = false;
+////                toBreakWorkstation = false;
 //            }
-
-
-            if (toBreakWorkstation && MinecraftClient.getInstance().currentScreen == null) {
-                workstation = world.getBlockState(VillagerTradingHallAssistant.workstation).getBlock();
-                if(!switchToEffectiveTool(world.getBlockState(VillagerTradingHallAssistant.workstation), player)){
-                    if(toolWarning) {
-                        MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("No effective tool in the hotbar! please put the effective tool for the workstation in your hotbar."));
-                        toolWarning = false;
-                    }
-                    return;
-                }
-                player.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES, new Vec3d(VillagerTradingHallAssistant.workstation.getX() + 0.5, VillagerTradingHallAssistant.workstation.getY()+0.5, VillagerTradingHallAssistant.workstation.getZ() + 0.5));
-                MinecraftClient.getInstance().options.attackKey.setPressed(true);
-//                Objects.requireNonNull(MinecraftClient.getInstance().interactionManager).attackBlock(VillagerTradingHallAssistant.workstation, Direction.UP);
-                hasBrokenBlock = false;
-//                toBreakWorkstation = false;
-            }
-
-
-            if (Objects.requireNonNull(world).getBlockState(VillagerTradingHallAssistant.workstation).getBlock() == Blocks.AIR && VillagerTradingHallAssistant.villager != null && VillagerTradingHallAssistant.villager.getVillagerData().getProfession() == VillagerProfession.NONE){
-                placeWorkstation(VillagerTradingHallAssistant.workstation, (BlockItem) workstation.asItem());//please never be weird lol
-            }
-
-
-        }
-        if(VillagerTradingHallAssistant.villager != null && VillagerTradingHallAssistant.workstation != null && world.getBlockState(VillagerTradingHallAssistant.workstation).getBlock() != Blocks.AIR && hasBrokenBlock){
+//
+//
+//            if (Objects.requireNonNull(world).getBlockState(VillagerTradingHallAssistant.workstation).getBlock() == Blocks.AIR && VillagerTradingHallAssistant.villager != null && VillagerTradingHallAssistant.villager.getVillagerData().getProfession() == VillagerProfession.NONE){
+//                placeWorkstation(VillagerTradingHallAssistant.workstation, (BlockItem) workstation.asItem());//please never be weird lol
+//            }
+//
+//
+//        }
 
 
 
-            VillagerEntity villager = VillagerTradingHallAssistant.villager;
 
 
-            if(villager.getVillagerData().getProfession() == VillagerProfession.LIBRARIAN && MinecraftClient.getInstance().currentScreen instanceof MerchantScreen) {
-                toBreakWorkstation = true;
-                for (TradeOffer offer : ((MerchantScreen) MinecraftClient.getInstance().currentScreen).getScreenHandler().getRecipes()) {
-                    ItemStack stack = offer.getSellItem();
-                    if (stack.getItem() == Items.ENCHANTED_BOOK) {
-                        Identifier enchant = new Identifier(((NbtCompound) EnchantedBookItem.getEnchantmentNbt(stack).get(0)).getString("id"));
-                        System.out.println(enchant);
-                        System.out.println("lvl: " + ((NbtCompound) EnchantedBookItem.getEnchantmentNbt(stack).get(0)).getInt("lvl"));
-                        if (Configs.ACCEPTABLE_ENCHANTMENTS.getStrings().contains(enchant.getPath()) && Objects.requireNonNull(Registry.ENCHANTMENT.get(enchant)).getMaxLevel()/*I SHOULD HOPE that this will never be null lmao*/ == ((NbtCompound) EnchantedBookItem.getEnchantmentNbt(stack).get(0)).getInt("lvl")) {
-                            MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("Found enchantment " + enchant.getPath()));
-                            toBreakWorkstation = false;
-                            toolWarning = true;
-                            Configs.ENABLE_MOD.resetToDefault();
-                        }
+        VillagerEntity villager = VillagerTradingHallAssistant.villager;
+
+
+        if(villager != null && MinecraftClient.getInstance().currentScreen instanceof MerchantScreen screen
+                && villager.getVillagerData().getProfession() == VillagerProfession.LIBRARIAN
+                && !screen.getScreenHandler().getRecipes().equals(trades)){
+//            screen.getScreenHandler().getRecipes().forEach(printOfferList);
+//            System.out.println();
+            toRefreshTrades = true;
+            hasRefreshedTrades = false;
+            trades = screen.getScreenHandler().getRecipes();
+//            System.out.println(trades.equals(screen.getScreenHandler().getRecipes()));
+            for (TradeOffer offer : (screen).getScreenHandler().getRecipes()) {
+                ItemStack stack = offer.getSellItem();
+                if (stack.getItem() == Items.ENCHANTED_BOOK) {
+                    Identifier enchant = new Identifier(((NbtCompound) EnchantedBookItem.getEnchantmentNbt(stack).get(0)).getString("id"));
+
+                    System.out.println("%s at lvl: %d cost: %d".formatted(enchant.toString(), ((NbtCompound) EnchantedBookItem.getEnchantmentNbt(stack).get(0)).getInt("lvl"), offer.getAdjustedFirstBuyItem().getCount()));
+                    if (Configs.ACCEPTABLE_ENCHANTMENTS.getStrings().contains(enchant.getPath()) &&
+                            Objects.requireNonNull(Registry.ENCHANTMENT.get(enchant)).getMaxLevel()/*I SHOULD HOPE that this will never be null lmao*/ == ((NbtCompound) EnchantedBookItem.getEnchantmentNbt(stack).get(0)).getInt("lvl")
+                            && offer.getAdjustedFirstBuyItem().getCount() <= Configs.MAX_COST.getIntegerValue()) {
+                        MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("Found enchantment " + enchant.getPath() + " at a cost of " + offer.getAdjustedFirstBuyItem().getCount() + " emeralds"));
+                        toRefreshTrades = false;
+//                            toolWarning = true;
+                        Configs.ENABLE_MOD.resetToDefault();
+                        trades = null;
                     }
                 }
-
-                if(toBreakWorkstation){
-                    MinecraftClient.getInstance().currentScreen.close();
-                }
             }
+
         }
+        if(toRefreshTrades && !hasRefreshedTrades && MinecraftClient.getInstance().currentScreen instanceof MerchantScreen screen && screen.getScreenHandler().getRecipes().equals(trades)){
+//            System.out.println("REFRESHING!");
+            //send the trade cycling packet
+            if (screen.getScreenHandler().isLeveled() && screen.getScreenHandler().getExperience() <= 0) {
+                TradeCyclingClientMod.sendCycleTradesPacket();
+//                mc.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+            }
+            hasRefreshedTrades = true;
+
+        }
+
+
+
 
     }
 
@@ -159,6 +192,12 @@ public class MainMod {
 
         return switched;
     }
+
+    public Consumer<? super TradeOffer> printOfferList = ((offer) ->
+    {
+        System.out.print("BUY: " + offer.getAdjustedFirstBuyItem().getItem() + (offer.getSecondBuyItem().isOf(Items.AIR) ? "" : "   BUYB: " + offer.getSecondBuyItem().getItem()) + "   SELL: " + offer.getSellItem().getItem());
+    }
+    );
 
 
 
